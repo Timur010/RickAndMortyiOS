86c9085c5b1865bcba606001bec8849fcc4ca7d1
Timur010 <duakar010@gmail.com>
2023-02-10 10:47:03 +0300

    added collection View

--- RickAndMorty/Controlles/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controlles/Core/RMCharacterViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 final class RMCharacterViewController: UIViewController {
-
+    let characterListView = CharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
-        
-        RMService.shared.execute(.listCharactersRequest, expecting: RMGetAllCharacterResponse.self) { result in
-            switch result {
-            case .success(let model):
-                print("Total: "+String(model.info.count))
-                print("Page result count: "+String(model.results.count))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+        view.addSubview(characterListView)
+        setUpView
+    }
+    
+    private func setUpView () {
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
