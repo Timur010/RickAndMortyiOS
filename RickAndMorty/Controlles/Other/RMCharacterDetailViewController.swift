c9d791eb6ced73b4e2534eb09e0507e8790de9c9
Timur010 <duakar010@gmail.com>
2023-02-16 09:55:01 +0300

    Part 12  Created detail view

--- RickAndMorty/Controlles/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controlles/Other/RMCharacterDetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
+    private let detailView = RMCharacterDetailView()
+    
     
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
@@ -23,6 +25,23 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTabShare))
+        addConstraint()
         
+    }
+    
+    @objc
+    private func didTabShare() {
+        
+    }
+    
+    private func addConstraint() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
